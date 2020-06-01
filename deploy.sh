1e3663f23dc0761af8ca8159ee4f098eec4ffc32
gbsnaker <longmenkezhai@sohu.com>
2020-06-01 18:29:26 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,20 @@
+cd /home/admin/application
+case $1 in 
+start) 
+nohup java -jar find-links.jar https://baidu.com & 
+echo $! > pid  
+break
+;;
+stop)
+pid=`cat pid`
+kill -9 $pid 
+echo > pid 
+break
+;;
+*)
+  echo "Usage ${0} start or stop"
+  ;;
 
-pwd
-ls
-echo "deploy jar to s3"
+
+
+
