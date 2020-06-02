a3a02928d8e95d75a50f7296a51742b02f909a19
gbsnaker <longmenkezhai@sohu.com>
2020-06-02 14:08:59 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,11 @@ start)
 cd /home/admin/application
 nohup java -jar find-links.jar https://baidu.com & 
 echo $! > pid  
-break
 ;;
 stop)
 pid=`cat pid`
 kill -9 $pid 
 rm -rf /home/admin/application
-break
 ;;
 *)
   echo "Usage ${0} start or stop"
